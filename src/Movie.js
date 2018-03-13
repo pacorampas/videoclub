46ff36d0e382beef2035c0ff2807de28983c9c4e
Paco Rampas <franciscoantonio.rampasvazquez@telefonica.com>
2018-03-13 10:13:50 +0100

    basic redux

--- src/Movie.js
+++ src/Movie.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import React, { PureComponent } from 'react';
 import PropTypes from 'prop-types'
 import { Link } from 'react-router-dom'
 
-const Movie = ({ match }) => (
-  <div>
-    <ul>
-      <li>
-        <Link to="/">Home</Link>
-      </li>
-    </ul>
-    <h2>Movie: {match.params.id}</h2>
-  </div>
-);
+import { connect } from 'react-redux'
+import { fetchStuff } from './store/actions'
+
+class Movie extends PureComponent {
+  componentWillMount() {
+    this.props.dispatch(fetchStuff())
+  }
+
+  render() {
+    const { match, stuff } = this.props
+
+    return(
+      <div>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+        </ul>
+        <h2>Movie: {match.params.id} {stuff.hi}</h2>
+      </div>
+    );
+  }
+}
 
 Movie.propTypes = {
+  dispatch: PropTypes.func.isRequired,
   match: PropTypes.shape({
     params: PropTypes.shape({
       id: PropTypes.string,
     }),
   }).isRequired,
+  stuff: PropTypes.shape({
+    hi: PropTypes.string,
+  }),
 };
 
-export default Movie;
+function mapStateToProps(state) {
+  return {
+    stuff: state.stuff,
+  };
+}
+
+export default connect(
+  mapStateToProps,
+)(Movie);
--- src/index.js
+++ src/index.js
@@ -1,9 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from 'react-router-dom'
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import { Provider } from 'react-redux'
+import configureStore from './store/configureStore'
+import './index.css'
+import App from './App'
+import registerServiceWorker from './registerServiceWorker'
 
-ReactDOM.render(<Router><App /></Router>, document.getElementById('root'));
+const store = configureStore();
+
+ReactDOM.render(
+  <Provider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </Provider>,
+  document.getElementById('root'));
 registerServiceWorker();
